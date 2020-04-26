edc17e19c9e8bb6e215d721debd8bbb547cea631
utpal-singh <utpal_singh@yahoo.com>
2020-04-26 20:34:38 +0530

    Logistic reg added and prev version deleted

--- logistic_reg.py
+++ logistic_reg.py
@@ -1,72 +1,60 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Apr 26 16:54:18 2020
+Created on Sun Apr 26 20:00:36 2020
 
 @author: utpal
 """
+
 import numpy as np
 
-class LogisticReg():
+class logistic_reg():
       """
+      
       A Logistic Regression Class
       
       """
       
-      def __init__(self, x, y, alp):
-            """
-            Args:
-                  m_test = Inputs a number of training examples
-                  No of feature of a training example is currently set to 2
-                  x: A feature matrix
-                  alp: learning rate
-                  y: a row matrix
-            
-            """
-            
-            self.alp = alp
+      def __init__(self, x, y):
+            self.x = x
             self.y = y
             
-      def _sigmoid(self, x):
-            return 1/(1+np.exp(-x))
-      
-      def GradDescent(self):
-            J = 0
-            dw_1 = 0
-            dw_2 = 0
-            db = 0
-            b = np.random.randint(low = 0, high = 1)
-            z = np.zeros(x.shape[0])
+            
+      def Grad_des(self):
             
             m = x.shape[1]
             n = x.shape[0]
             
+            J = 0
+            db = 0
+            
+            
+            z = np.zeros(m)
+            a = np.zeros(m)
+            dz = np.zeros(m)
+            dw = np.zeros((n,1))
             
             for i in range(m):
-                  w = np.random.randn(n, 1)
-                  dz = np.random.randn(n, 1)
-                  dw = np.random.randn(m, 1)
-                  
-                  
-                  a = np.random.randn(m,1)
-                  
-                  z[i] = np.dot(w.T, x[:, [i]]) + b
-#                        z[i] = w*x[:, [i]]
+                  w = np.random.randn(n,1)
+                  z[i] = np.dot(w.T, x[:, [i]])
                   a[i] = self._sigmoid(z[i])
-                  J += -(y[i]*(np.log(a[i]) + (1-y[i])*(np.log(1-a[i]))))
+                  
+                  J+= -(y[i]*np.log(a[i]) + (1-y[i])*np.log(1-a[i]))
+                  
                   dz[i] = a[i] - y[i]
                   
-                  for j in range(n):
-                        dw[j] += x[j][i]*dz[i]
-                  db += dz[i]
+                  db += db + dz[i]
                   
-            J = J/m
-            print(J)
-            
-            
-if __name__ == '__main__':
-      x = np.array([[1,2,3,4],[2,3,4,5],[3,4,5,6]])
-      y = np.array([4,5,6,7])
-      alp = 2
-      logreg = LogisticReg(x,y,alp)
-      logreg.GradDescent()
+                  for j in range(n):
+                        dw[j] += np.dot(x[j], dz)
+                  
+      def _sigmoid(self, z):
+            return 1/(1+np.exp(-z))
+                  
+if __name__ == "__main__":
+      x = np.array([[.1,.2,.3,.4], [.5,.6,.7,.8], [.9,.10,.11,.12]])
+      y = np.array([.13, .14, .15, .16])
+      logre = logistic_reg(x,y)
+      logre.Grad_des()
+                  
+                  
       
